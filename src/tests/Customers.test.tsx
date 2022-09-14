d8ebfdfd7be8894d093d1ef44deb069a5de7dd65
amkera <amber_kerawala@berkeley.edu>
2022-09-14 11:05:09 -0400

    Comment out unfinished code

--- src/tests/Customers.test.tsx
+++ src/tests/Customers.test.tsx
@@ -4,7 +4,7 @@ import mockAxios from "axios"
 
 jest.mock("axios") //not sure if I can use this since I used the React SDK & not axios
 //unclear what data is
-mockAxios.get.mockImplementation(() => Promise.resolve({ data: {} }))
+// mockAxios.get.mockImplementation(() => Promise.resolve({ data: {} }))
 
 describe("Customers Page", () => {
   it("should perform get request to the API", async () => {
